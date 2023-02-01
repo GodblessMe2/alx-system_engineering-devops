9b185f6fef4a5c7ef43d88ddf241e9bec277f47b
GodblessMe2 <oshindeinde@gmail.com>
2023-02-01 15:25:36 +0100

    corrections

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # Fixes bad PHP extension in the WordPress file 'wp-setting.php'
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g/var/www/html/wp-setting.php',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'Debugging':
+  command  => 'sudo sed -i \'s/phpp/php/g\' /var/www/html/wp-settings.php && sudo service apache2 restart',
+  provider => shell,
 }
